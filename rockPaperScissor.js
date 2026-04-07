7238c07ac8d252978b3b5a2c94107d44675be444
topSimpa <mohammedabdulmujeeb16@gmail.com>
2026-04-07 18:05:35 +0100

    New version of rock-paper-scissors
    Created a GUI for the game rock-paper scissor on branch rps-ui

--- rockPaperScissor.js
+++ rockPaperScissor.js
@@ -1,5 +1,3 @@
-// console.log("Hello World")
-
 function getComputerChoice() {
     let computerChoice = Math.floor(Math.random() * 3);
     // console.log(computerChoice);
@@ -14,61 +12,78 @@ function getComputerChoice() {
 }
 
 
-function getHumanChoice() {
-    let humanChoice = prompt("Rock, Paper or Scissors");
-    humanChoice = humanChoice.toLowerCase()
-    // console.log(humanChoice);
-    return humanChoice;
-}
 
-getHumanChoice()
 
 function playGame() {
     let humanScore = 0;
     let computerScore = 0;
+    const selectionOptions = document.querySelectorAll("button");
+    const result = document.querySelector("#result");
+    const game = document.createElement("div");
+    const body = document.querySelector("body");
+    
 
+    body.appendChild(game);
 
     function playRound(humanChoice, computerChoice) {
+        if ((humanScore + computerScore) == 0) //to start a newgame
+            game.textContent = "";
+
         if (computerChoice === "paper") {
             if (humanChoice === "rock") {
                 computerScore += 1;
-                console.log( `You loose! ${computerChoice} beats ${humanChoice}`);
+                result.textContent = `You loose! ${computerChoice} beats ${humanChoice}`;
+                result.style.color = "red";
             } else if (humanChoice === "scissors") {
                 humanScore += 1;
-                console.log( `You Win! ${humanChoice} beats ${computerChoice}`);
+                result.textContent = `You Win! ${humanChoice} beats ${computerChoice}`;
+                result.style.color = "green";
             } else 
-                console.log("Tie")   
+                result.textContent = "Tie!"  
         } else if (computerChoice === "rock") {
             if (humanChoice === "scissors") {
                 computerScore += 1;
-                console.log( `You loose! ${computerChoice} beats ${humanChoice}`);
+                result.textContent = `You loose! ${computerChoice} beats ${humanChoice}`;
+                result.style.color = "red";
             } else if (humanChoice === "paper") {
                 humanScore += 1;
-                console.log( `You Win! ${humanChoice} beats ${computerChoice}`);
+                result.textContent = `You Win! ${humanChoice} beats ${computerChoice}`;
+                result.style.color = "green";
             } else 
-                console.log("Tie")     
+                result.textContent = "Tie!"    
         } else {
             if (humanChoice === "paper") {
                 computerScore += 1;
-                console.log( `You loose! ${computerChoice} beats ${humanChoice}`);
+                result.textContent = `You loose! ${computerChoice} beats ${humanChoice}`;
+                result.style.color = "red";
             } else if (humanChoice === "rock") {
                 humanScore +=1
-                console.log( `You Win! ${humanChoice} beats ${computerChoice}`);    
+                result.textContent = `You Win! ${humanChoice} beats ${computerChoice}`;    
+                result.style.color = "green";
             } else
                 console.log("Tie");
         }
+
+        //To End a game
+        if (humanScore == 5) {
+            game.textContent = `You won the game ${humanScore} to ${computerScore}`;
+            humanScore = 0;
+            computerScore = 0;
+        } else if (computerScore == 5) {
+            game.textContent = `Computer wins ${computerScore} to ${humanScore}`
+            humanScore = 0;
+            computerScore = 0;
+        }
     }
+    
 
-    playRound(getHumanChoice(), getComputerChoice());
-    playRound(getHumanChoice(), getComputerChoice());
-    playRound(getHumanChoice(), getComputerChoice());
-    playRound(getHumanChoice(), getComputerChoice());
-    playRound(getHumanChoice(), getComputerChoice());
+    selectionOptions.forEach((button) => {
+        button.addEventListener('click', () => {           
+            playRound(button.id, getComputerChoice());
+        });
+    
+    });
 
-    if (humanScore > computerScore)
-        console.log("Champion! You Won!");
-    else 
-        console.log("Game lost! Computer Won")
 }
 
 playGame();
